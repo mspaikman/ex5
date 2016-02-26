befe959df4ee85ed688c931b89d3d2b0140a0f52
mspaikman <sd@gmail.com>
2016-02-25 17:29:52 -0800

    initial commit

---
+++ js/ex5.js
@@ -0,0 +1,14 @@
+$('.content').sortable();
+$('.collection .container').draggable({
+	  connectToSortable: '.content',
+	  helper: 'clone',
+	});
+
+$('.collection').droppable({
+  
+  drop: function(e, ui) {
+    if( $( ui.draggable ).parent().hasClass( 'content' ) )  {
+     $(ui.draggable).remove(); 
+    }
+  }
+});
